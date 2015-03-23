31a4417e3b9de83e5e52df3e5ff2204685c9ea0b
Neyaz <ne9zzz@gmail.com>
2015-03-23 22:28:52 +0500

    admin + ckeditor

---
+++ app/admin/product.rb
@@ -0,0 +1,25 @@
+ActiveAdmin.register Product do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :price
+      f.input :description, :as => :ckeditor
+    end
+    f.actions
+  end
+
+end
